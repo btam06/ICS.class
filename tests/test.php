13046d493998d29552b72a92484aba1b5ec15f99
Brian Tam <brian@imarc.com>
2017-06-05 17:34:20 -0700

    use correct namespace and timezone in tests.

--- tests/test.php
+++ tests/test.php
@@ -1,11 +1,12 @@
 <?php
+include('../vendor/autoload.php');
 include('../Ics.php');
 try {
-    $ics = new vOutput\Ics(array(
+    $ics = new Itzamna\Ics(array(
         'organizer'   => 'noreply@imarc.net',
         'uid'         => NULL,
         'prodid'      => NULL,
-        'timezone'    => 'US-Eastern',
+        'timezone'    => 'Eastern',
         'start_date'  => '2016-04-07 5:00:00',
         'end_date'    => '2016-04-07 6:00:00',
         'summary'     => 'test',
